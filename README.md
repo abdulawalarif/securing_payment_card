111f39b3aa1e98064153d5e2a18fb43046fdc32c
abdul awal <awal@twintechsoft.com>
2024-11-05 18:54:36 +0600

    added hook for accessing lifcycle

---
+++ README.md
@@ -0,0 +1,77 @@
+
+# Network Request with Bloc
+
+## This Flutter and Dart project demonstrates how to send network requests using the Bloc library. Here I fetched data from two different API endpoints and displays is on a list, This stracture also optimized performance through caching to prevent double network call.
+
+**The project provides a solid foundation for developers looking to implement network-based features in their applications using Flutter’s Bloc architecture. It includes features like data loading from multiple sources, caching of responses, and efficient UI updates when data changes.**
+
+
+
+## Features
+* **Hiding payment card:** When user tap and ...
+
+## File stracture
+
+    
+    ├── lib
+    │   ├── main.dart                  
+     
+ 
+
+ 
+ <img src="demo/bloc.gif" width="50%" alt="Demo of this application" />
+
+
+ 
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/abdulawalarif/securing_payment_card.git
+```
+
+Go to the project directory
+
+```bash
+  cd securing_payment_card
+```
+
+Install dependencies
+
+```bash
+  flutter pub get
+```
+
+Connect a physical device or start a virtual device on your machine
+
+```bash
+  flutter run
+```
+
+
+
+## How to tweak this project for your own uses
+
+
+## Reporting Bugs or Requesting Features?
+
+If you found an issue or would like to submit an improvement to this project,
+please submit an issue using the issues tab above. If you would like to submit a PR with a fix, reference the issue you created!
+
+##  Known Issues and Future Work
+
+
+
+## Author
+
+- [@abdulawalarif](https://github.com/abdulawalarif)
+  
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+
+
+ 
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Securing Payment'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.lock),
+                text: 'Secured Card',
+              ),
+              Tab(
+                icon: Icon(Icons.lock_open),
+                text: 'Card Is open',
+              ),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            CardSecured(),
+            CardOpen(),
+          ],
+        ),
       ),
-      home: Scaffold(
-      )
+    );
+  }
+}
+
+
+class CardSecured extends HookWidget {
+  const CardSecured({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final state = useAppLifecycleState();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Opacity(
+              opacity: state == AppLifecycleState.resumed ? 1.0 : 0.0,
+              child: Container(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 10,
+                      color: Colors.black.withAlpha(100),
+                      spreadRadius: 10,
+                    ),
+                  ],
+                ),
+                child: Image.asset('assets/image_of_card.png'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CardOpen extends StatelessWidget {
+  const CardOpen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 10,
+                    color: Colors.black.withAlpha(100),
+                    spreadRadius: 10,
+                  ),
+                ],
+              ),
+              child: Image.asset('assets/image_of_card.png'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
